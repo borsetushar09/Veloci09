6fedf3241a0d6bb6d87f68f9fd17bda6ee18c522
borsetushar09 <98937560+borsetushar09@users.noreply.github.com>
2022-02-03 15:23:40 +0530

    Update ActitimeHeader.java

--- ActitimeAppplication/src/main/java/pom/ActitimeHeader.java
+++ ActitimeAppplication/src/main/java/pom/ActitimeHeader.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ActitimeHeader {
-	
+	//sainath//
 	private WebDriver driver2;
 	  
 	@FindBy(xpath="//a[@class='content users']")
